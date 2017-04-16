omadoww/browser-extension

commit 5f1d3b2c647a294256aacdb75edfc24aff25a861

Extract common parseBlob() from plugins (#341)
* docker: rename `image` property to `target`

This makes it more consistent with the other plugins.

* Extract common parseBlob() from plugins

Since so many of the plugins have the same `parseBlob` implementation
(just with a different set of regexes), let's make it so that the
plugins can just specify the regexes to use with `insertLink`

* Let a plugin's `getLinkRegexes` return a single regex

See https://github.com/OctoLinker/browser-extension/pull/341#pullrequestreview-31934713

lib/plugins/vim.js
@@ -1,7 +1,6 @@
 import ghShorthand from 'github-url-from-username-repo';
 import giturl from 'giturl';
 import { VIM_PLUGIN } from '../../packages/helper-grammar-regex-collection/index.js';
-import insertLink from '../insert-link';
 
 export default class Vim {
 
@@ -40,10 +39,7 @@ export default class Vim {
     };
   }
 
-  parseBlob(blob) {
-    insertLink(blob.el, VIM_PLUGIN, {
-      pluginName: this.constructor.name,
-      shorthand: '$1',
-    });
+  static getLinkRegexes() {
+    return VIM_PLUGIN;
   }
 }

lib/octo-linker.js
@@ -3,6 +3,7 @@ import notification from './notification';
 import BlobReader from '../packages/blob-reader';
 import clickHandler from './click-handler';
 import Plugins from './plugin-manager.js';
+import insertLink from './insert-link';
 import debugMode from './debug-mode.js';
 import loadPlugins from './load-plugins';
 import * as storage from './options/storage.js';
@@ -32,7 +33,17 @@ function run(self) {
     }
 
     plugins.forEach((plugin) => {
-      plugin.parseBlob(blob);
+      if (plugin.parseBlob) {
+        plugin.parseBlob(blob);
+      } else if (plugin.constructor.getLinkRegexes) {
+        [].concat(plugin.constructor.getLinkRegexes(blob)).forEach((regex) => {
+          insertLink(blob.el, regex, {
+            pluginName: plugin.constructor.name,
+            target: '$1',
+            path: blob.path,
+          });
+        });
+      }
     });
   });
 }

lib/plugins/css.js
@@ -1,5 +1,4 @@
 import { CSS_IMPORT } from '../../packages/helper-grammar-regex-collection/index.js';
-import insertLink from '../insert-link';
 import relativeFile from '../resolver/relative-file.js';
 
 export default class CSS {
@@ -20,11 +19,7 @@ export default class CSS {
     };
   }
 
-  parseBlob(blob) {
-    insertLink(blob.el, CSS_IMPORT, {
-      pluginName: this.constructor.name,
-      target: '$1',
-      path: blob.path,
-    });
+  static getLinkRegexes() {
+    return CSS_IMPORT;
   }
 }

lib/plugins/docker.js
@@ -1,13 +1,12 @@
 import { DOCKER_FROM } from '../../packages/helper-grammar-regex-collection/index.js';
-import insertLink from '../insert-link';
 
 export default class Docker {
 
-  static resolve({ image }) {
+  static resolve({ target }) {
     let isOffical = true;
-    const imageName = image.split(':')[0];
+    const imageName = target.split(':')[0];
 
-    if (image.includes('/')) {
+    if (target.includes('/')) {
       isOffical = false;
     }
 
@@ -26,10 +25,7 @@ export default class Docker {
     };
   }
 
-  parseBlob(blob) {
-    insertLink(blob.el, DOCKER_FROM, {
-      pluginName: this.constructor.name,
-      image: '$1',
-    });
+  static getLinkRegexes() {
+    return DOCKER_FROM;
   }
 }

lib/plugins/gemfile-manifest.js
@@ -1,5 +1,4 @@
 import { GEM } from '../../packages/helper-grammar-regex-collection/index.js';
-import insertLink from '../insert-link';
 import liveResolverQuery from '../resolver/live-resolver-query.js';
 
 export default class Rubygems {
@@ -18,10 +17,7 @@ export default class Rubygems {
     };
   }
 
-  parseBlob(blob) {
-    insertLink(blob.el, GEM, {
-      pluginName: this.constructor.name,
-      target: '$1',
-    });
+  static getLinkRegexes() {
+    return GEM;
   }
 }

lib/plugins/go.js
@@ -1,6 +1,5 @@
 import { join, dirname } from 'path';
 import { go } from '../../packages/helper-grammar-regex-collection/index.js';
-import insertLink from '../insert-link';
 
 function goFile({ path, target }) {
   const list = [];
@@ -63,13 +62,7 @@ export default class Go {
     };
   }
 
-  parseBlob(blob) {
-    go(blob.toString()).forEach((val) => {
-      insertLink(blob.el, val, {
-        pluginName: this.constructor.name,
-        target: '$1',
-        path: blob.path,
-      });
-    });
+  static getLinkRegexes(blob) {
+    return go(blob.toString());
   }
 }

lib/plugins/haskell.js
@@ -1,5 +1,4 @@
 import { HASKELL_IMPORT } from '../../packages/helper-grammar-regex-collection/index.js';
-import insertLink from '../insert-link';
 import githubSearch from '../resolver/github-search.js';
 
 export default class Haskell {
@@ -24,11 +23,7 @@ export default class Haskell {
     };
   }
 
-  parseBlob(blob) {
-    insertLink(blob.el, HASKELL_IMPORT, {
-      pluginName: this.constructor.name,
-      target: '$1',
-      path: blob.path,
-    });
+  static getLinkRegexes() {
+    return HASKELL_IMPORT;
   }
 }

lib/plugins/homebrew-manifest.js
@@ -1,5 +1,4 @@
 import { HOMEBREW } from '../../packages/helper-grammar-regex-collection/index.js';
-import insertLink from '../insert-link';
 import Ruby from './ruby';
 import relativeFile from '../resolver/relative-file.js';
 
@@ -26,11 +25,7 @@ export default class Homebrew {
     return Ruby.getPattern();
   }
 
-  parseBlob(blob) {
-    insertLink(blob.el, HOMEBREW, {
-      pluginName: this.constructor.name,
-      path: blob.path,
-      target: '$1.rb',
-    });
+  static getLinkRegexes() {
+    return HOMEBREW;
   }
 }

commit 5aaa7e6d0458b10db48903b0226a66607730fccf

vim: Use `target` argument instead of `shorthand` (#347)
Commit 5f1d3b2 accidentally broke vimrc support, but this fixes it.

Test page: https://github.com/gmarik/vimfiles/blob/1f4f26d42f54443f1158e0009746a56b9a28b053/vimrc#L140-L144

lib/plugins/vim.js
@@ -5,21 +5,21 @@ import { VIM_PLUGIN } from '../../packages/helper-grammar-regex-collection/index
 export default {
   name: 'Vim',
 
-  resolve({ shorthand }) {
+  resolve({ target }) {
     // Logic adapted from https://github.com/VundleVim/Vundle.vim/blob/11fdc428fe741f4f6974624ad76ab7c2b503b73e/doc/vundle.txt#L196
-    const components = shorthand.split('/');
+    const components = target.split('/');
 
     // GitHub is used when a user/repo is passed to `Plugin`.
     if (components.length === 2) {
-      return ghShorthand(shorthand, true);
+      return ghShorthand(target, true);
     }
     // Any single word without a slash '/' is assumed to be from Vim Scripts.
     if (components.length === 1) {
-      return ghShorthand(`vim-scripts/${shorthand}`, true);
+      return ghShorthand(`vim-scripts/${target}`, true);
     }
     // Assume it's a URL otherwise. We can't link to git/ssh, so change to https
     // and hope it works.
-    return giturl.parse(shorthand);
+    return giturl.parse(target);
   },
 
   getPattern() {

test/plugins/vim.test.js
@@ -4,21 +4,21 @@ import vimPlugin from '../../lib/plugins/vim';
 describe('vim-plugin', () => {
   it('resolves VundleVim/Vundle.vim to https://github.com/VundleVim/Vundle.vim', () => {
     assert.deepEqual(
-      vimPlugin.resolve({ shorthand: 'VundleVim/Vundle.vim' }),
+      vimPlugin.resolve({ target: 'VundleVim/Vundle.vim' }),
       'https://github.com/VundleVim/Vundle.vim',
     );
   });
 
   it('resolves ctrlp.vim to https://github.com/vim-scripts/ctrlp.vim', () => {
     assert.deepEqual(
-      vimPlugin.resolve({ shorthand: 'ctrlp.vim' }),
+      vimPlugin.resolve({ target: 'ctrlp.vim' }),
       'https://github.com/vim-scripts/ctrlp.vim',
     );
   });
 
   it('resolves a git:// URL to https/http', () => {
     assert.deepEqual(
-      vimPlugin.resolve({ shorthand: 'git://git.wincent.com/command-t.git' }),
+      vimPlugin.resolve({ target: 'git://git.wincent.com/command-t.git' }),
       'http://git.wincent.com/command-t',
     );
   });